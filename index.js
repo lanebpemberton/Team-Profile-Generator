570edc952fd2e21a2f495e143dcb7726a246fcf5
Lane Pemberton <lanebpemberton@gmail.com>
2021-05-01 12:51:21 -0400

    importing classes into index

---
+++ index.js
@@ -0,0 +1,18 @@
+//import employee classes
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+//temp code to delete later
+let manager = new Manager("Lane","24","lanebpemberton@gmail.com","7062014493");
+
+console.log(manager.getName());
+console.log(manager.getId());
+console.log(manager.getEmail());
+console.log(manager.getRole());
+console.log(manager.getOfficeNumber());
+//setup functions that write data to html
+
+//create inquire questions
+
+//do something with answers and write to html
